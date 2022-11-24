adc18bc5ff566e4ca71a7164edb998ed98c3d0b4
Asiern <asnulorri@gmail.com>
2022-11-24 07:25:23 +0100

    add: static ip

--- src/wireless.cpp
+++ src/wireless.cpp
@@ -1,19 +1,25 @@
 #include "wireless.h"
 
+#ifdef _STATIC_IP
+IPAddress ip(192, 168, 1, 3);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+#endif
+
 void initWiFi()
 {
     WiFi.mode(WIFI_STA);
     WiFi.begin(SSID, PASSWD);
+
+#ifdef _STATIC_IP
+    WiFi.config(ip, gateway, subnet);
+#endif
+
     Serial.println("Connecting to WiFi");
 
     while (WiFi.status() != WL_CONNECTED)
         delay(1000);
 
-#ifdef IP_STATIC
-        // IPAddress ip(IP);
-        // IPAddress gateway(GATEWAY);
-        // IPAddress subnet(SUBNET);
-        // WiFi.config(ip,GATEWAY,SUBNET),
-#endif
+    // #endif
     Serial.println(WiFi.localIP());
 }
